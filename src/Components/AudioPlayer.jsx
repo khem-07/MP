18a8d311ab21af5d252c821f21198cfb6c03fca1
khem-07 <khem53580@gmail.com>
2025-05-08 19:34:34 +0530

    font css

--- src/Components/AudioPlayer.jsx
+++ src/Components/AudioPlayer.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import Wave from "react-wavify";
 import { FaPlay, FaPause } from "react-icons/fa";
 import "../starryBackground.scss";
+import './font.css';
 
 const STREAM_URL = "https://listen.ramashamedia.com:8330/stream";
 const SONGTITLE_API = "/api/currentsong"; // Proxy endpoint
